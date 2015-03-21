e450ea89013c7173c77db62a2bf263327b2f11be
André Felipe Dias <andref.dias@pronus.eng.br>
2015-03-21 19:19:05 -0300

    Mantendo teste para fim de registro. Mas não é confiável. Às vezes funciona... Posteriormente, valeria um estudo mais detalhado.

---
+++ test/test_daemon.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+import os
+import sh
+from time import sleep
+from os.path import isfile, abspath, dirname, join
+
+default_interval = 0.1
+config_filename = '/tmp/config.txt'
+output_filename = '/tmp/current_time.txt'
+app_filename = abspath(join(dirname(__file__), '../app.py'))
+
+
+def get_pid():
+    pid = str(sh.grep(sh.tail('-n', '40', '/var/log/syslog'), '/app.py: pid:')).split()[-1]
+    return int(pid.split()[-1])
+
+
+def set_interval(interval):
+    with open(config_filename, 'w') as f:
+        f.write(str(interval))
+    return
+
+
+def setup():
+    if isfile(output_filename):
+        os.remove(output_filename)
+    set_interval(default_interval)
+    out = sh.env('python', app_filename)
+    assert len(out) > 0
+    return
+
+
+def teardown():
+    pid = get_pid()
+    sh.kill(pid)
+    return
+
+
+class TestDaemon(object):
+
+    def test_launch(self):
+        assert isfile(output_filename)
+        os.remove(output_filename)
+        sleep(default_interval * 3)
+        assert isfile(output_filename)
+
+    def test_reload(self):
+        set_interval(1)
+        pid = get_pid()
+        sh.kill('-s', 'SIGUSR1', pid)  # reload
+        log = str(sh.grep(sh.tail('/var/log/syslog'), 'app.py: intervalo = 1.0s'))
+        assert len(log) > 0
